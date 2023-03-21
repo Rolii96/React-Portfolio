998f0eec37da5da39ad4e72dcabda255d76f499b
Roland <79096928+Rolii96@users.noreply.github.com>
2023-03-21 16:43:54 +0100

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -286,6 +286,35 @@ const Work = () => {
               </div>
             </div>
           </div>
+          
+          <div
+            style={{ backgroundImage: `url(${Project10})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/*Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <div>
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  New Movie Website
+                </span>
+                <div className="pt-8 text-center">
+                  <a
+                    href="https://roland-ajeti.netlify.app/"
+                    target="_blank"
+                  >
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      GO LIVE
+                    </button>
+                  </a>
+                  <a href="https://github.com/Rolii96" target="_blank">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
